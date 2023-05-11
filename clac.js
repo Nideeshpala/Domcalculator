f6fabf1feda73e094dedd92607a477725e0602fc
Nideesh p <nideeshp1997@gmail.com>
2023-05-12 00:46:02 +0530

    j

---
+++ clac.js
@@ -0,0 +1,12 @@
+function displayData(n){
+    t1.value+=n
+}
+function evaluateOpe(){
+    t1.value=eval(t1.value)
+}
+function allClear(){
+    t1.value=""
+}
+function slicem(){
+   t1.value=t1.value.slice(0,-1)
+}
